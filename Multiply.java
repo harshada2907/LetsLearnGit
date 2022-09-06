1d941db50671eedb53fedde9ddaa61930acc6e75
harshada <harshadadh2907@gmail.com>
2022-09-06 20:02:13 +0530

    added Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -2,7 +2,7 @@ public class Multiply {
     public static void main(String[] args) {
         int a = 5;
         int b = 8;
-        System.out.println("The product is" +(a*b));
+        System.out.println("the product is " + (a*b));
     }
     
 }
